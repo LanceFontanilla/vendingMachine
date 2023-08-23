40e5609397bfcff2fbc6b1ec55c05fc5b0f919a1
LanceFontanilla <lancefontanilla@gmail.com>
2023-08-22 23:14:33 -0600

    some more functionality. still in progress

--- app/controllers/VendorsController.js
+++ app/controllers/VendorsController.js
@@ -1,26 +1,29 @@
 
-import { AppState } from "../AppState.js";
-import { vendorsService } from "../services/VendorsService.js";
-import { setHTML } from "../utils/Writer.js";
+import { AppState } from "../AppState.js"
+import { vendorsService } from "../services/VendorsService.js"
+import { setHTML } from "../utils/Writer.js"
 
 
 export class VendorsController {
 
     constructor() {
         console.log('Time to Vend')
+        this.drawMoney()
+        this.drawSnackList()
+
         // this.drawSnackList()
 
+        AppState.on('money', this.drawMoney)
 
-        AppState.on('totalMoney', this.drawMoney)
     }
     drawSnackList() {
         const snacks = AppState.snacks
         let listContent = ''
         snacks.forEach(snack => listContent += snack.ListTemplate)
+        setHTML('listContent', listContent)
         console.log(listContent)
-        document.getElementById('snack-list').innerHTML = listContent
-        setHTML('snack-list', listContent)
     }
+
 
     addMoney() {
         vendorsService.addMoney()
@@ -28,10 +31,17 @@ export class VendorsController {
 
     drawMoney() {
         console.log('drawing your money')
+        setHTML('money', `<div id = "money"> $${AppState.money.toFixed(2)}</div> `)
+    }
 
-        document.getElementById('totalMoney').innerText = `Total Money: ${AppState.money} `
-
-
+    buySnack() {
+        console.log('buying snack')
+        vendorsService.buySnack(snackName)
+        let snackContent = ''
+        const mySnacks = AppState.snacks
+        mySnacks.push(snackName)
+        setHTML('mySnacks', mySnacks)
+        console.log(mySnacks)
     }
 
 }
